9e7a34f81a33fd3564551ebf7ba519194255e398
Dominic Chingoma <dominic.chingoma@trustgro.com>
2022-06-26 23:52:15 +0300

    loading tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
  
 module.exports = {
   content: [
+    "./src/**/*.{html,js}",
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './containers/**/*.{js,ts,jsx,tsx}',
