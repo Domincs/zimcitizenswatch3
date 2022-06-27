f1db167256395dcee4e65ed8972bb42e17c98e27
Dominic Chingoma <dominic.chingoma@trustgro.com>
2022-06-27 11:15:18 +0300

    fixing errors with links

--- containers/overall_summary.js
+++ containers/overall_summary.js
@@ -8,8 +8,8 @@ import { ScrollSection } from "./scroll-section";
 export function OverallSummaryContainer({ summary }) {
     const [scrolled, setScrolled] = useState(0)
     const items = [
-        { name: "Malawi", active: scrolled < 33.3 , link: '/malawi', target: '__self', map: "/maps/mw.svg" },
-        { name: "Zambia", active: scrolled > 33.3 && scrolled < 66.6, link: '/zambia', target: '__self', map: "/maps/zm.svg" },
+        { name: "Malawi", active: scrolled < 33.3 , link: '/malawi', target: '_self', map: "/maps/mw.svg" },
+        { name: "Zambia", active: scrolled > 33.3 && scrolled < 66.6, link: '/zambia', target: '_self', map: "/maps/zm.svg" },
         { name: "Zimbabwe", active: scrolled > 66.6, link: 'https://zimcitizenswatch.org', target: '_blank', map: "/maps/zw.svg" }
     ]
 
@@ -30,7 +30,7 @@ export function OverallSummaryContainer({ summary }) {
                     <img src={items.find((obj) => obj.active === true).map} className="h-[16em] mb-8 pr-4" />
                     {/* <a className="rounded-full px-8 py-2 leading-none tracking-[.1em] text-xs uppercase font-serif text-white bg-orange" href={items.find((obj) => obj.active === true).link} target={items.find((obj) => obj.active === true).target}> */}
                     <Button color="orange">
-                        <a className="flex flex-row gap-4 text-[10px]" href={items.find((obj) => obj.active === true).link} target={items.find((obj) => obj.active === true).target} {...(items.find((obj) => obj.active === true).link.includes("https://zimcitizenswatch") && { rel: "noreferrer" })}>
+                        <a className="flex flex-row gap-4 text-[10px]" href={items.find((obj) => obj.active === true).link} target={items.find((obj) => obj.active === true).target} {...(items.find((obj) => obj.active === true).target === '_blank' && { rel: "noreferrer" })}>
                         <span>Visit {items.find((obj) => obj.active === true).name} Tracker</span><img src="/icons/right-arrow-white.svg" className="h-[0.7em]" />
                         </a>
                     </Button>
